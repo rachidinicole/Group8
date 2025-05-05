6562c1e0544255919907dad443e368a35889f2f4
NicoleRachidi <135596460+rachidinicole@users.noreply.github.com>
2025-05-05 15:48:18 +0200

    HomePage.js

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-4xl font-bold">Home Page</h1>
+      <p className="text-lg mt-4">You are logged in!</p>
+    </div>
+  );
+}
